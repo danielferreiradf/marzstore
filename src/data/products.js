128c4cfe10ab071a47a882456544c4e59ee3fa31
danielferreiradf <danielferreiradk@gmail.com>
2020-03-04 11:44:10 +0000

    added fake data to product.js file

---
+++ src/data/products.js
@@ -0,0 +1,124 @@
+export const pants = [
+  {
+    id: 1,
+    name: "Dark brown Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/541578227/zoom/1.jpg",
+    price: 30.0
+  },
+  {
+    id: 2,
+    name: "Brown Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/546701271/zoom/1.jpg",
+    price: 25.0
+  },
+  {
+    id: 3,
+    name: "Black Fitness Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/542148093/zoom/1.jpg",
+    price: 35.0
+  }
+];
+
+export const jeans = [
+  {
+    id: 1,
+    name: "Jeans Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/550938474/zoom/1.jpg",
+    price: 35.0
+  },
+  {
+    id: 2,
+    name: "Skinny Jeans Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/550884980/zoom/1.jpg",
+    price: 45.0
+  },
+  {
+    id: 3,
+    name: "Plane Jeans Pants",
+    imageUrl: "https://img.lojasrenner.com.br/item/550936671/zoom/1.jpg",
+    price: 42.0
+  }
+];
+
+export const tshirts = [
+  {
+    id: 1,
+    name: "Playstation T-shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/549331376/zoom/10.jpg",
+    price: 13.0
+  },
+  {
+    id: 2,
+    name: "Blue Globe T-shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/550914464/zoom/7.jpg",
+    price: 17.0
+  },
+  {
+    id: 3,
+    name: "Pink T-shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/550337071/zoom/8.jpg",
+    price: 15.0
+  }
+];
+export const shirts = [
+  {
+    id: 1,
+    name: "Red Shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/549784893/zoom/10.jpg",
+    price: 20.0
+  },
+  {
+    id: 2,
+    name: "Green Shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/549766695/zoom/10.jpg",
+    price: 18.0
+  },
+  {
+    id: 3,
+    name: "Dark Blue Shirt",
+    imageUrl: "https://img.lojasrenner.com.br/item/549655346/zoom/10.jpg",
+    price: 16.0
+  }
+];
+
+export const jackets = [
+  {
+    id: 1,
+    name: "Brown Jacket",
+    imageUrl: "https://img.lojasrenner.com.br/item/547473362/zoom/1.jpg",
+    price: 40.0
+  },
+  {
+    id: 2,
+    name: "Windbreaker Jacket",
+    imageUrl: "https://img.lojasrenner.com.br/item/549838409/zoom/1.jpg",
+    price: 45.0
+  },
+  {
+    id: 3,
+    name: "Jeans Jacket",
+    imageUrl: "https://img.lojasrenner.com.br/item/550886862/zoom/3.jpg",
+    price: 35.0
+  }
+];
+
+export const shorts = [
+  {
+    id: 1,
+    name: "Black Shorts",
+    imageUrl: "https://img.lojasrenner.com.br/item/538918841/zoom/1.jpg",
+    price: 20.0
+  },
+  {
+    id: 2,
+    name: "Plane Shorts",
+    imageUrl: "https://img.lojasrenner.com.br/item/534967523/zoom/1.jpg",
+    price: 15.0
+  },
+  {
+    id: 3,
+    name: "Sports Gray Shorts",
+    imageUrl: "https://img.lojasrenner.com.br/item/548111291/zoom/1.jpg",
+    price: 16.0
+  }
+];
